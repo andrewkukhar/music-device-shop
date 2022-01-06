58d6853465baa860f376b0082cffdeb56889cc37
andreyrise <andrey.rise@gmail.com>
2022-01-06 20:13:43 +0200

    change style for few blocks

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,6 +1,6 @@
 (function () {
-    const iconBurger = document.querySelector("section nav .header_navigation-burger");
-    const navigationsBody = document.querySelector("section nav .header_navigation-wrap");
+    const iconBurger = document.querySelector(".header__nav-burger");
+    const navigationsBody = document.querySelector(".header__nav-wrap");
     iconBurger.addEventListener("click", function (e) {
       document.body.classList.toggle("_lock");
       iconBurger.classList.toggle("active");
